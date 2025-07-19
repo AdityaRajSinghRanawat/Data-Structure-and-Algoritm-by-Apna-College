392c916aca6937dc07f5cfd64f21c6722a00b2e7
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-07-19 16:30:48 +0530

    done pointers

--- 16. Pointers/10. Output Qs/first.cpp
+++ 16. Pointers/10. Output Qs/first.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    int arr[] = {10, 20, 30, 40};
+    int *ptr = arr;
+
+    cout << *(ptr + 1) << endl; // index 1 = 20 
+    cout << *(ptr + 3) << endl; // index 3 = 40
+    ptr++; // ptr + 1 = index 1
+    cout << *ptr << endl; // index 1 = 20
     return 0;
 }
