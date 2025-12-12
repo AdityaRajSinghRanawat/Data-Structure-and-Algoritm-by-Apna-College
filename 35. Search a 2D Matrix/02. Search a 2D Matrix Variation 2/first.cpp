b09fe2148486d92cb306b1a6ba4fb31335dac516
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-12-12 18:22:34 +0530

    practiced the search in 2d matrix variation 2

--- 35. Search a 2D Matrix/02. Search a 2D Matrix Variation 2/first.cpp
+++ 35. Search a 2D Matrix/02. Search a 2D Matrix Variation 2/first.cpp
@@ -9,7 +9,32 @@ int main()
         Search a 2D Matrix II
     */
 
-    
+    class Solution
+    {
+    public:
+        bool searchMatrix(vector<vector<int>> &matrix, int target)
+        {
+            int m = matrix.size(), n = matrix[0].size();
+            int row = 0, col = n - 1;
+
+            while (col >= 0 && row <= m - 1)
+            { // (row < m) ====> (row <= m - 1)
+                if (target == matrix[row][col])
+                {
+                    return true;
+                }
+                else if (target < matrix[row][col])
+                { // left
+                    col--;
+                }
+                else if (target > matrix[row][col])
+                { // down
+                    row++;
+                }
+            }
+            return false;
+        }
+    };
 
     return 0;
 }
